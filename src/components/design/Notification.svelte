85157bfae32ee104b76ddfeae668f06a95832b4a
DevXian <coolman555@me.com>
2022-05-17 18:41:24 +0900

    feat: notification form complete

--- src/components/design/Notification.svelte
+++ src/components/design/Notification.svelte
@@ -1,15 +1,50 @@
 <script type="ts">
+	import { card } from './config/color';
 	import { css } from '@emotion/css';
+
+	let toggle = false;
 
 	const notificationStyle = css`
 		color: white;
-		font-size: 28px;
-		float: right;
+		font-size: 100%;
+		width: 45px;
+		height: 45px;
 		background-color: transparent;
 		border: none;
+		padding: 6px;
+		background-color: rgb(40, 40, 45);
+		border-radius: 50%;
+		box-shadow: 1px 3px 12px 1px rgb(28, 28, 35);
+		cursor: pointer;
 	`;
+
+	const notificationBoxStyle = css`
+		position: relative;
+		float: right;
+		text-align: right;
+	`;
+
+	const notificiationListStyle = css`
+		position: absolute;
+		background-color: ${card};
+		border-radius: 10px;
+		padding: 12px;
+		margin-top: 12px;
+		right: 0;
+		box-shadow: -10px 50px 100px -70px black;
+	`;
+
+	const toggleHandle = () => {
+		toggle = !toggle;
+	};
 </script>
 
-<button class={notificationStyle}>
-	<i class="ri-notification-line" />
-</button>
+<div class={notificationBoxStyle}>
+	<button class={notificationStyle} on:click={toggleHandle}>
+		<i class="ri-notification-line" />
+	</button>
+
+	{#if toggle}
+		<div class={notificiationListStyle}>Something...</div>
+	{/if}
+</div>
