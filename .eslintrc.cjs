a2a91ec6ef1390e25562238d199691a1f68f9a22
SeokHyeon Jang <coolman555@me.com>
2023-04-28 16:17:19 +0900

    init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,14 @@
 module.exports = {
-	root: true,
-	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
-	parserOptions: {
-		sourceType: 'module',
-		ecmaVersion: 2020
-	},
-	env: {
-		browser: true,
-		es2017: true,
-		node: true
-	}
-};
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': 'warn',
+  },
+}
--- .lintstagedrc.cjs
+++
@@ -1,5 +0,0 @@
-module.exports = {
-	'*.{js,ts,json,svelte}': ['yarn format', 'yarn lint'],
-	'./src/**/*.{svelte,ts}': ['yarn format', 'yarn lint'],
-	'./.{ts,cjs,js}': ['yarn format', 'yarn lint']
-};
