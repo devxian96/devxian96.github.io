6093b818cf04c679c78d260185a02184502b9c7b
DevXian <coolman555@me.com>
2023-09-16 12:27:18 +0900

    init

--- src/pages/Home.tsx
+++
@@ -1,5 +0,0 @@
-import type { FC } from 'react';
-
-export const Home: FC = () => {
-	return <div>hi</div>;
-};
--- src/pages/Skill.tsx
+++
@@ -1,5 +0,0 @@
-import type { FC } from 'react';
-
-export const Skill: FC = () => {
-	return <div>Skill</div>;
-};
